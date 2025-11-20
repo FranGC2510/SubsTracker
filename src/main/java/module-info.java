0c9c4eb8c7947d29862924faf8ff8ef42e6d4724
Francisco Javier García Cañero <franciscoj.garciacan@gmail.com>
2025-11-20 16:35:09 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.dam.fcojavier.substracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.dam.fcojavier.substracker to javafx.fxml;
+    exports org.dam.fcojavier.substracker;
+}
---
+++ src/main/java/org/dam/fcojavier/substracker/Launcher.java
@@ -0,0 +1,9 @@
+package org.dam.fcojavier.substracker;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
